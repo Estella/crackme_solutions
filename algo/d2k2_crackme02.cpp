c3f27cadea8e44c4e8244cef6e5a356a3ef3aca2
mudlord <mudlord@rebote.net>
2018-10-25 07:43:07 +1100

    Done solution to crackme no. 2

--- algo/d2k2_crackme02.cpp
+++ algo/d2k2_crackme02.cpp
@@ -3,14 +3,42 @@
 void process_serial(char *name, char *serial_out)
 {
 	unsigned char buffer[32]={0};
-	byte bufctr=0;
-	char tabl[] = "SJKAZBVTECGIDFNG";
+	unsigned char ctr1_buf[8]={0};
+	char tabl[] = "SJKAZBVTECGIDFNG ";
 	int namelen = strlen(name);
-	for (int ctr=0; ctr <namelen;ctr++)
-	{
+	int ctr=0,bufctr=0;
+	for (ctr=0, bufctr=0; ctr <namelen;ctr++,bufctr+=2)
 		tabl[bufctr]=toupper(name[ctr]);
-		bufctr+=2;
-	}
+
+	int ctr1 = 0;
+	for (ctr=0; ctr <0x10;ctr++)
+		ctr1 += tabl[ctr];
+	ctr1 *= (namelen * 0xFF);
+	ctr1 ^= 0xACEBDFAB;
+	ctr1 = ((ctr1>>24)&0xff) | 
+		((ctr1<<8)&0xff0000) | 
+		((ctr1>>8)&0xff00) | 
+		((ctr1<<24)&0xff000000);
+	wsprintf((char*)ctr1_buf,"%1X", ctr1);
+
+
+     ctr=0;
+     while (ctr != 8)
+     {
+	  if(ctr1_buf[ctr] < 0x3A)
+		ctr1_buf[ctr] = ctr1_buf[ctr]+ 0x11;
+	  ctr++;
+     }
+
+	 ctr=0;
+	 bufctr=0;
+	 while (bufctr != 0x10)
+	 {
+		 tabl[bufctr+1]=ctr1_buf[ctr]+5;
+		 bufctr+=2;
+		 ctr++;
+	 }
+
 	wsprintf(serial_out,"%s", tabl);
 }
 
