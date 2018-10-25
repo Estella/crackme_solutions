7a03903a7eb2582a3e5dcbbede0bd6646b9b1c43
mudlord <mudlord@rebote.net>
2018-10-25 17:19:16 +1100

    better.

--- algo/d2k2_crackme02.cpp
+++ algo/d2k2_crackme02.cpp
@@ -14,8 +14,7 @@ Then use Scylla to rebuild imports, etc.
 
 void process_serial(char *name, char *serial_out)
 {
-	unsigned char buffer[32]={0};
-	unsigned char ctr1_buf[8]={0};
+	unsigned char magic_buf[8]={0};
 	char tabl[] = "SJKAZBVTECGIDFNG";
 	int namelen = strlen(name);
 	int ctr=0,bufctr=0,magic_dword = 0;
@@ -25,29 +24,29 @@ void process_serial(char *name, char *serial_out)
 		bufctr+=2;
 	}
 
-    ctr=0;
+	ctr=0;
 	while(ctr !=0x10)
 		magic_dword += tabl[ctr++];
-		
+
 	magic_dword *= (namelen * 0xFF);
 	magic_dword ^= 0xACEBDFAB;
 	magic_dword = _byteswap_ulong(magic_dword);
-	wsprintf((char*)ctr1_buf,"%1X", magic_dword);
+	wsprintf((char*)magic_buf,"%1X", magic_dword);
 
-    ctr=0;
-    while (ctr != 8)
+	ctr=0;
+	while (ctr != 8)
 	{
-	  if(ctr1_buf[ctr] < 0x3A)
-	    ctr1_buf[ctr] = (ctr1_buf[ctr]+ 0x11);
-	  ctr++;
-    }
+		if(magic_buf[ctr] < 0x3A)
+			magic_buf[ctr] = (magic_buf[ctr]+ 0x11);
+		ctr++;
+	}
 
 	ctr=0;
 	bufctr=0;
 	while (bufctr != 0x10)
 	{
-	  tabl[bufctr+1]=ctr1_buf[ctr++]+5;
-	  bufctr+=2;
+		tabl[bufctr+1]=magic_buf[ctr++]+5;
+		bufctr+=2;
 	}
 	wsprintf(serial_out,"%s", tabl);
 }
