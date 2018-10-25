00a6650e894d8f60ff2b4b68d05033216c38df53
mudlord <mudlord@rebote.net>
2018-10-26 09:30:14 +1100

    Added crackme 3.

---
+++ algo/d2k2_crackme03.cpp
@@ -0,0 +1,21 @@
+#include <windows.h>
+
+/*
+This particular crackme is MSDOS based. 
+You will need DOSBox and Turbo Debugger
+as well as UNP.
+*/
+
+void process_serial(char *name, char *serial_out)
+{
+	int namelen = strlen(name);
+    namelen--;
+    int magic_dword = name[namelen-1];
+    magic_dword *= 0x40;
+    magic_dword *= namelen;
+    magic_dword += 0x400;
+    magic_dword *= namelen;
+	wsprintf((char*)serial_out,"%d", magic_dword);
+}
+
+
