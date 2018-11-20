638f5df0ea22235196554fe0170922ff24590862
mudlord <mudlord@rebote.net>
2018-11-21 08:29:00 +1100

    Clean up buffer to make it easier to understand.

--- algo/d2k2_crackme04.cpp
+++ algo/d2k2_crackme04.cpp
@@ -2,7 +2,8 @@
 #include <stdio.h>
 void process_serial(char *name, char *serial_out)
 {
-    BYTE serial_buf[9] = { 0x23, 0x23, 0x23, 0x23, 0x23, 0x23, 0x23, 0x23,0x0 };
+    BYTE serial_buf[9] = { '0', '0','0', '0', '0', '0', '0', '0',0x0 };
+    const int check_dword = 0xFFFFE3B0 ^ 0x8EF21BD;
     //pad buf with zero in case it fails.
     bool done = false;
     int num_ser = 0;
@@ -12,10 +13,10 @@ void process_serial(char *name, char *serial_out)
     {
         //the bruteforce
         (*ser_ptr)++;
-        if (*ser_ptr == '[')
+        if (*ser_ptr == 'Z')
         {
             //limit range to usable ASCII keyspace
-            *ser_ptr++ = '#';
+            *ser_ptr++ = '0';
             if (*ser_ptr == 0)
             {
                 fclose(pFile);
@@ -35,7 +36,7 @@ void process_serial(char *name, char *serial_out)
                 regconst *= regconst;
             }
             //right regconst?
-            if (regconst == 0xf710c5c4)
+            if (regconst == check_dword)
             {
                 fprintf(pFile, "%s\n", serial_buf);
                 num_ser++;
