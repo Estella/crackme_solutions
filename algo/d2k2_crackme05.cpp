07be48c7b86e27f0359be50e4ea5945694b66f9f
mudlord <mudlord@rebote.net>
2019-01-08 08:07:40 +1100

    start golfing. fore!

--- algo/d2k2_crackme05.cpp
+++ algo/d2k2_crackme05.cpp
@@ -1,40 +1,35 @@
 #include <windows.h>
-
+#define setlobyte(val, b)val = (DWORD(val) & 0xFFFFFF00) | (DWORD(b) & 0xFF)
 void process_serial(char *name, char *serial_out)
 {
 	unsigned char buffer_4034AB[32] = { 0 };
 	memcpy(buffer_4034AB, name, strlen(name));
-	unsigned char buffer_004050F0[32] = { 0 };
-	int eax = strlen(name);
-	buffer_004050F0[0] = eax;
-	int ecx = 0;
-	int ebx = 0;
-	int edx = 0;
-
-	while (ecx < eax)
+	int namelen = strlen(name);
+	
+	for (int ctr = 0; ctr < namelen; ctr++)
 	{
-	ebx = buffer_4034AB[ecx];
-	ebx += eax;
-	edx = *(DWORD *)buffer_4034AB;
-	edx = edx ^ ebx;
-	buffer_4034AB[ecx] = (BYTE)edx;
-	ecx++;
+		int ebx = buffer_4034AB[ctr];
+		ebx += namelen;
+		int edx = *(DWORD *)buffer_4034AB;
+		edx ^= ebx;
+		buffer_4034AB[ctr] = (BYTE)edx;
 	}
 
-	ecx = 0;
-	eax = buffer_004050F0[0];
-	edx = 0x68F6B76C;
-	ebx = 0x10101010;
+	int ecx = 0;
+	int edx;
+	int eax;
+	eax = namelen;
+	int ebx = 0x10101010;
 hashloop:
-	ebx |= (buffer_4034AB[ecx] << 0);
+	eax = namelen;
+	setlobyte(ebx, buffer_4034AB[ecx]);
 	ebx <<= 5;
+	edx = 0x68F6B76C;
 	edx *= eax;
 	ebx *= eax;
 	edx ^= ebx;
 	edx += *(DWORD *)buffer_4034AB;
 	*(DWORD *)buffer_4034AB = edx;
-
-
 	//call hash function
 
 
