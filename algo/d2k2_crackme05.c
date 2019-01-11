2edfa579d05d8ce1e0833663bd8cee16cac63807
mudlord <mudlord@rebote.net>
2019-01-12 06:52:21 +1100

    work on finishing first loop of crackme 5.

--- algo/d2k2_crackme05.c
+++ algo/d2k2_crackme05.c
@@ -6,38 +6,57 @@ void process_serial(char *name, char *serial_out)
 	unsigned char buffer_4034AB[32] = { 0 };
 	unsigned char buffer_hashbuf[64] = { 0 };
 	DWORD* hashbuf_ptr = (DWORD*)buffer_hashbuf;
+	BYTE *hashbuf_ptr_b = (BYTE*)hashbuf_ptr;
 	memcpy(buffer_4034AB, name, strlen(name));
 	int namelen = strlen(name);
 	
-	for (int ctr = 0; ctr < namelen; ctr++)
+	for (int ecx = 0; ecx < namelen; ecx++)
 	{
-		int ebx = buffer_4034AB[ctr];
+		int ebx = buffer_4034AB[ecx];
 		ebx += namelen;
 		int edx = *(DWORD *)buffer_4034AB;
 		edx ^= ebx;
-		buffer_4034AB[ctr] = (BYTE)edx;
+		buffer_4034AB[ecx] = (BYTE)edx;
 	}
 
-	int ecx = 0;
+	
 	int edx;
-	int eax;
-	eax = namelen;
+	int eax = namelen;
 	int ebx = 0x10101010;
-hashloop:
-	(((BYTE *)&ebx)[0]) = buffer_4034AB[ecx];
-	ebx <<= 5;
-	edx = 0x68F6B76C;
-	edx *= eax;
-	ebx *= eax;
-	edx ^= ebx;
-	edx += *(DWORD *)buffer_4034AB;
-	*(DWORD *)buffer_4034AB = edx;
-	//call hash function
-	d2dk_crackme05_hash((DWORD*)buffer_4034AB, namelen, hashbuf_ptr);
-	((BYTE *)hashbuf_ptr) += 6;
-	ebx = (*hashbuf_ptr);
-	edx ^= ebx;
-	edx = _rotl(edx, 7);
-	d2dk_crackme05_hash((DWORD*)buffer_4034AB, namelen, hashbuf_ptr);
+	int ecx = 0;
+
+	while (ecx != namelen)
+	{
+		(((BYTE *)&ebx)[0]) = buffer_4034AB[ecx];
+		ebx <<= 5;
+		edx = 0x68F6B76C;
+		edx *= eax;
+		ebx *= eax;
+		edx ^= ebx;
+		edx += *(DWORD *)buffer_4034AB;
+		*(DWORD *)buffer_4034AB = edx;
+		//call hash function
+		d2dk_crackme05_hash((DWORD*)buffer_4034AB, namelen, hashbuf_ptr);
+		hashbuf_ptr_b += 6;
+		ebx = *(DWORD*)hashbuf_ptr_b;
+		edx ^= ebx;
+		edx = _rotl(edx, 7);
+		d2dk_crackme05_hash((DWORD*)buffer_4034AB, namelen, hashbuf_ptr);
+		hashbuf_ptr_b = (char*)hashbuf_ptr;
+		hashbuf_ptr_b += 8;
+		ebx = *(DWORD*)hashbuf_ptr_b;
+		edx ^= ebx;
+		buffer_4034AB[ecx] = (((BYTE *)&ebx)[0]);
+		d2dk_crackme05_hash((DWORD*)buffer_4034AB, namelen, hashbuf_ptr);
+		hashbuf_ptr_b = (char*)hashbuf_ptr;
+		hashbuf_ptr_b += 10;
+		ebx = *(DWORD*)hashbuf_ptr_b;
+		edx ^= ebx;
+		edx = _rotl(edx, 4);
+		buffer_4034AB[ecx] = (((BYTE *)&edx)[0]);
+		d2dk_crackme05_hash((DWORD*)buffer_4034AB, namelen, hashbuf_ptr);
+		ebx = 0x10101010;
+		ecx++;
+	}
 	wsprintf(serial_out, "%s", name);
 }
