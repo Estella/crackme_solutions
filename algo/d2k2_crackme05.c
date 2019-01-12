b71b3d76ddbde2c4b4d77773bbb5f7cf6b377891
mudlord <mudlord@rebote.net>
2019-01-13 07:37:50 +1100

    Found a bug in the crackme code. It makes a redundant hashing call, which only wastes CPU time.

--- algo/d2k2_crackme05.c
+++ algo/d2k2_crackme05.c
@@ -14,7 +14,6 @@ void hexprint(unsigned char* pin,int buflength,unsigned char* str)
 	*pout++ = hex[(*pin >> 4) & 0xF];
 	*pout++ = hex[(*pin) & 0xF];
 	*pout = 0;
-
 }
 
 void process_serial(char *name, char *serial_out)
@@ -23,11 +22,11 @@ void process_serial(char *name, char *serial_out)
 	unsigned char buffer_hashbuf[64] = { 0 };
 	unsigned char hash_formatted[64] = { 0 };
 	DWORD* hashbuf_ptr = (DWORD*)buffer_hashbuf;
-	BYTE *hashbuf_ptr_b = (BYTE*)hashbuf_ptr;
+	
 	memcpy(buffer_4034AB, name, strlen(name));
 	int namelen = strlen(name);
-	
-	for (int ctr = 0; ctr < namelen; ctr++)
+	int ctr = 0;
+	for (ctr;ctr < namelen; ctr++)
 	{
 		int letter = buffer_4034AB[ctr] + namelen;
 		int letter_xor = *(DWORD *)buffer_4034AB;
@@ -35,12 +34,14 @@ void process_serial(char *name, char *serial_out)
 		buffer_4034AB[ctr] = (BYTE)letter_xor;
 	}
 	
-	for(int ctr = 0; ctr != namelen;ctr++)
+	ctr = 0;
+	while (ctr != namelen)
 	{
+		BYTE *hashbuf_ptr_b = (BYTE*)hashbuf_ptr;
 		int ebx = 0x10101010;
+		int edx = 0x68F6B76C;
 		ebx = ((DWORD)ebx & 0xFFFFFF00) | (DWORD)buffer_4034AB[ctr] & 0xFF;
 		ebx <<= 5;
-		int edx = 0x68F6B76C;
 		edx *= namelen;
 		ebx *= namelen;
 		edx ^= ebx;
@@ -48,17 +49,18 @@ void process_serial(char *name, char *serial_out)
 		*(DWORD *)buffer_4034AB = edx;
 		//call hash function
 		d2dk_crackme05_hash((DWORD*)buffer_4034AB, namelen, hashbuf_ptr);
+		hexprint(buffer_hashbuf, 16, hash_formatted);
 		hashbuf_ptr_b += 6;
 		ebx = *(DWORD*)hashbuf_ptr_b;
 		edx ^= ebx;
 		edx = _rotl(edx, 7);
-		d2dk_crackme05_hash((DWORD*)buffer_4034AB, namelen, hashbuf_ptr);
 		hashbuf_ptr_b = (char*)hashbuf_ptr;
 		hashbuf_ptr_b += 8;
 		ebx = *(DWORD*)hashbuf_ptr_b;
 		edx ^= ebx;
 		buffer_4034AB[ctr] = LOBYTE(edx);
 		d2dk_crackme05_hash((DWORD*)buffer_4034AB, namelen, hashbuf_ptr);
+		hexprint(buffer_hashbuf, 16, hash_formatted);
 		hashbuf_ptr_b = (char*)hashbuf_ptr;
 		hashbuf_ptr_b += 10;
 		ebx = *(DWORD*)hashbuf_ptr_b;
@@ -68,6 +70,7 @@ void process_serial(char *name, char *serial_out)
 		d2dk_crackme05_hash((DWORD*)buffer_4034AB, namelen, hashbuf_ptr);
 		//debug print
 		hexprint(buffer_hashbuf, 16, hash_formatted);
+		ctr++;
 	}
 	hexprint(buffer_hashbuf, 16, hash_formatted);
 	wsprintf(serial_out, "%s", hash_formatted);
