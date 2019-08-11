90baae12b674a7061bcdc17726a56fb5d3dfa3ca
mudlord <mudlord@rebote.net>
2019-08-11 19:40:36 +1000

    keygenned byteptr's crackme.

--- algo/byteptr_kgnme1.c
+++ algo/byteptr_kgnme1.c
@@ -2,7 +2,7 @@
 #include <wincrypt.h>
 #include <stdbool.h>
 
-void MD5(BYTE * data, ULONG len, BYTE* rgbhash)
+void MD5(BYTE * data, ULONG len, BYTE* hash_data)
 {
 	HCRYPTPROV hProv = 0;
 	HCRYPTPROV hHash = 0;
@@ -10,7 +10,7 @@ void MD5(BYTE * data, ULONG len, BYTE* rgbhash)
 	CryptCreateHash(hProv, CALG_MD5, 0, 0, &hHash);
 	CryptHashData(hHash, data, len, 0);
 	DWORD cbHash = 16;
-	CryptGetHashParam(hHash, HP_HASHVAL, rgbhash, &cbHash, 0);
+	CryptGetHashParam(hHash, HP_HASHVAL, hash_data, &cbHash, 0);
 	CryptDestroyHash(hHash);
 	CryptReleaseContext(hProv, 0);
 }
@@ -19,11 +19,28 @@ void process_serial(char *name, char *serial_out)
 {
 	BYTE hash_bytes[16] = { 0 };
 	char hash_string[33] = { 0 };
+	int namelen = lstrlen(name);
+	MD5((BYTE*)name, namelen, (BYTE*)hash_bytes);
+	for (int i = 0; i < 4; i++)
+	{
+		DWORD* bufptr = hash_bytes;
+		DWORD bytes = *(DWORD*)(bufptr+i);
+		*(DWORD*)(bufptr + i) = _byteswap_ulong(bytes);
+	}
 
-	MD5((BYTE*)name, lstrlen(name), (BYTE*)hash_bytes);
-	for (int i = 0; i < 16; ++i)
-		wsprintf(&hash_string[i * 2], "%02x", (unsigned int)hash_bytes[i]);
-
+	BYTE* bufptr = hash_bytes;
+	DWORD EAX_ = *(DWORD*)(bufptr);
+	DWORD EBX_ = *(DWORD*)(bufptr+4);
+	EAX_ ^= EBX_;
+	wsprintf(&hash_string[0], "%.8X", EAX_);
+	EBX_ ^= 0xFBD0099;
+	wsprintf(&hash_string[8], "%.8X", EBX_);
+	DWORD ECX_ = *(DWORD*)(bufptr + 8);
+	ECX_ ^= EBX_;
+	wsprintf(&hash_string[16], "%.8X", ECX_);
+	EBX_ ^= 0xB0C0A09;
+	DWORD EDX_ = *(DWORD*)(bufptr + 0x0C);
+	wsprintf(&hash_string[24], "%.8X", EDX_);
 	wsprintf(serial_out, "%s",hash_string);
 }
 
