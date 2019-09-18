3967f76514a81c67c8974c0602dc88b695306ce2
mudlord <mudlord@rebote.net>
2019-09-18 21:46:53 +1000

    Finished keygen.

--- algo/d2k2_crackme09.c
+++ algo/d2k2_crackme09.c
@@ -1,3 +1,7 @@
+//https://en.wikipedia.org/wiki/Vigen%C3%A8re_cipher
+
+
+
 #include <windows.h>
 #include <stdbool.h>
 #include <stdint.h>
@@ -34,21 +38,15 @@ unsigned findcharintab(char* table,int let)
 
 void process_serial(char* name, char* serial_out)
 {
-	
-	unsigned char nameshit[] =   "FUCKYOU";
-	unsigned char serialshit[] = "D1CZSHI";
-	int strlen_serialshit = strlen(serialshit);
-	int strlen_nameshit = strlen(nameshit);
-	/*
-	for (int i = 0; i < strlen(nameshit); i++)
-	{
-		nameshit[i] = validchar_tbl[rand() % serialen];
-	}*/
+	int strlen_serialshit = strlen(name);
+	char* serialshit_compute = (char*)malloc(strlen_serialshit + 1);
+	char* serialshit = (char*)malloc(strlen_serialshit + 1);
+	memset(serialshit, 0, strlen_serialshit + 1);
+	memset(serialshit_compute, 0, strlen_serialshit + 1);
 
-	int shitvar1 = strlen(nameshit) << 2;
+	int shitvar1 = strlen_serialshit << 2;
 	if (shitvar1 >= 0x3C)shitvar1 = 0x1E;
 	unsigned char* shitstring1 = buildsertable(shitvar1,validchar_tbl);
-	wsprintf(serial_out, "%s", shitstring1);
 	int strlen_shitstring1 = strlen(shitstring1);
 	int shit1_len = strlen_shitstring1;
 
@@ -56,7 +54,7 @@ void process_serial(char* name, char* serial_out)
 	uint8_t DL= 0;
 	AL.rx = 0;
 
-	DL = nameshit[0];
+	DL = name[0];
 	DL = _rotl8(DL, 3);
 
 
@@ -64,23 +62,26 @@ void process_serial(char* name, char* serial_out)
 	for (int i = 0; ; i++)
 	{
 		int j = i + 1;
-		AL.b.lo = nameshit[i];
-		AL.b.lo ^= nameshit[j];
+		AL.b.lo = name[i];
+		AL.b.lo ^= name[j];
 		AL.b.lo += DL;
 		DL += AL.b.lo;
 		int res2 = AL.b.lo % strlen_shitstring1;
 		AL.b.lo = shitstring1[res2];
-		serialshit[i] = AL.b.lo;
-		if (j == strlen_nameshit)break;
+		serialshit_compute[i] = AL.b.lo;
+		if (j == strlen_serialshit)break;
 	}
 
-	for (int i = 0; i < strlen_nameshit; i++)
+	for (int i = 0; i < strlen_serialshit; i++)
 	{
-		int offset = findcharintab(shitstring1,serialshit[i]);
+		int offset = findcharintab(shitstring1,serialshit_compute[i]);
 		unsigned char* shitstring = buildsertable(offset,shitstring1);
+		int offset2 = findcharintab(shitstring1, name[i]);
+		serialshit[i] = shitstring[offset2];
 		free(shitstring);
 	}
 	free(shitstring1);
-
-	wsprintf(serial_out, "%s", "TEST");
+	free(serialshit_compute);
+	wsprintf(serial_out, "%s", serialshit);
+	free(serialshit);
 }
